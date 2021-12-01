240701bb03dc2664195a064a02b2c60af6fbbd5c
rramnani <36605356+rramnani@users.noreply.github.com>
2021-11-30 18:10:07 -0600

    Spotify app v1

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,10 @@
+import { atom } from "recoil";
+
+export const playlistState = atom ({
+    key: "playlistState",
+    default: null,
+});
+export const playlistIdState = atom ({
+    key: "playlistIdState",
+    default: "3kJ0b3xSJ5f9GxsDY4Q8K3",
+});
